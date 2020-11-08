7e1b7178860e7573d72e25e11b5fd69d9345ae04
Daniel Castro <danicc097@gmail.com>
2020-11-08 21:00:36 +0100

    Solver test

--- solver/shortest_operation.py
+++ solver/shortest_operation.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
